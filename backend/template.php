eb4048331a1fceb750f0019f6caa5570812e456d
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-04-11 15:58:30 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -7,19 +7,12 @@ if ($_SERVER['HTTP_HOST'] == 'odal-jk') {
     $version = mt_rand(10000, 99999999);
 } else {
     $getFileVersion = file($root."/backend/versionFiles.txt", FILE_IGNORE_NEW_LINES);
-    $version_now = $getFileVersion[0];
-    $version_new = $getFileVersion[1];
+    $version = $getFileVersion[0];
 
-    if ($version != $version_new) {
-        $version = $version_new;
-        $getFileVersion[0] = $getFileVersion[1];
-        file_put_contents($root."/backend/versionFiles.txt", implode(PHP_EOL, $getFileVersion));
-
-        clearCash($root . "/assets", $version);
-        clearCash($root . "/components", $version);
-        clearCash($root . "/pages", $version);
-        clearCash($root . "/plugins/modal", $version);
-    }
+    clearCash($root . "/assets", $version);
+    clearCash($root . "/components", $version);
+    clearCash($root . "/pages", $version);
+    clearCash($root . "/plugins/modal", $version);
 }
 
 if (file_exists("$root/static/meta.json")) {
