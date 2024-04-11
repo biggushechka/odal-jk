ad7b9b6da87f3a808c8ae0e11354da7870cc470e
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-04-11 18:03:09 +0300

    update

--- pages/successfully/index.php
+++
@@ -1,8 +0,0 @@
-<?
-include $_SERVER['DOCUMENT_ROOT'].'/backend/controller.php';
-$templates->head();
-$templates->links_head(["css"=>['/pages/successfully/successfully']])?>
-
-<div id="app"></div>
-
-<?$templates->script_head(["js"=>['/pages/successfully/index']])?>
