8890b3fa60952edad10120e7ea732f1c90a4ee93
Max <gorbatenkomax@yandex.ru>
2024-05-22 20:12:34 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -7,13 +7,21 @@ if ($_SERVER['HTTP_HOST'] == 'odal-jk') {
     $version = mt_rand(10000, 99999999);
 } else {
     $getFileVersion = file($root."/backend/versionFiles.txt", FILE_IGNORE_NEW_LINES);
-    $version = $getFileVersion[0];
+    $version_now = $getFileVersion[0];
+    $version_new = $getFileVersion[1];
+    $version = $version_now;
 
+    if ($version_now != $version_new) {
+        $version_now = $version_new;
+        $version = $version_now;
+        $getFileVersion[0] = $getFileVersion[1];
+        file_put_contents($root."/backend/versionFiles.txt", implode(PHP_EOL, $getFileVersion));
 
-    clearCash($root . "/assets", $version);
-    clearCash($root . "/components", $version);
-    clearCash($root . "/pages", $version);
-    clearCash($root . "/plugins/modal", $version);
+        clearCash($root . "/assets", $this->v);
+        clearCash($root . "/components", $this->v);
+        clearCash($root . "/pages", $this->v);
+        clearCash($root . "/plugins/modal", $this->v);
+    }
 }
 
 if (file_exists("$root/static/meta.json")) {
